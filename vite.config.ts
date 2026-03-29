d21b6992115da6c2b733e210d61e48a50d6d8599
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2026-03-29 00:04:30 -0500

    Fixes vercel issue with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  test: {
-    globals: false,
-    environment: "node",
-    include: ["src/**/*.test.ts"],
-  },
 });
